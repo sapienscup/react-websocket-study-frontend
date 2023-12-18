fb414777f1156cebbca6c5c79266667e5d64d46b
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2023-12-18 11:42:00 -0300

    Add

--- app/api/graphql.ts
+++ app/api/graphql.ts
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { get_api_host, get_api_port, get_api_protocol, get_app_env } from '@/envs'
+import { get_api_host, get_api_port, get_api_protocol } from '@/envs'
 
 import { split, HttpLink } from '@apollo/client'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { createClient } from 'graphql-ws'
-// import { WebSocketLink } from '@apollo/client/link/ws'
+import { WebSocketLink } from '@apollo/client/link/ws'
 
 const uri = {
   http: `${get_api_protocol()}://${get_api_host()}:${get_api_port()}/graphql`,
@@ -16,7 +16,11 @@ const httpLink = new HttpLink({
   uri: uri.http
 })
 
-let splitLink
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: uri.ws
+  })
+)
 
 // const wsLink = new WebSocketLink({
 //   uri: uri.ws,
@@ -30,27 +34,14 @@ let splitLink
 // * A function that's called for each operation to execute
 // * The Link to use for an operation if the function returns a "truthy" value
 // * The Link to use for an operation if the function returns a "falsy" value
-if (get_app_env() === 'production') {
-  splitLink = split(({ query }) => {
+const splitLink = split(
+  ({ query }) => {
     const definition = getMainDefinition(query)
     return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-  }, httpLink)
-} else {
-  const wsLink = new GraphQLWsLink(
-    createClient({
-      url: uri.ws
-    })
-  )
-
-  splitLink = split(
-    ({ query }) => {
-      const definition = getMainDefinition(query)
-      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-    },
-    httpLink,
-    wsLink
-  )
-}
+  },
+  wsLink,
+  httpLink
+)
 
 const gqlClient = new ApolloClient({
   link: splitLink,
